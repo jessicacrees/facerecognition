f3684a1f0826c01040195738d24aed5cb04d07a0
Jessica Crees <jessicacrees@gmail.com>
2013-05-31 08:13:14 +0100

    Face Detection
    Gone through <http://fewtutorials.bravesites.com/tutorials> tutorials up
    to face recognition.

---
+++ FaceRecognitionTest/FaceRecognitionTest/CameraCapture.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace FaceRecognitionTest
+{
+    partial class CameraCapture
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.CamImageBox = new Emgu.CV.UI.ImageBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbCamIndex = new System.Windows.Forms.ComboBox();
+            this.comboBoxScIncRte = new System.Windows.Forms.ComboBox();
+            this.comboBoxMinNeigh = new System.Windows.Forms.ComboBox();
+            this.textBoxWinSize = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.groupBoxTuneDetectParam = new System.Windows.Forms.GroupBox();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.pbExtractedFaces = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.CamImageBox)).BeginInit();
+            this.groupBoxTuneDetectParam.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbExtractedFaces)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CamImageBox
+            // 
+            this.CamImageBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.CamImageBox.Location = new System.Drawing.Point(7, 12);
+            this.CamImageBox.Name = "CamImageBox";
+            this.CamImageBox.Size = new System.Drawing.Size(418, 298);
+            this.CamImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.CamImageBox.TabIndex = 2;
+            this.CamImageBox.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Enabled = false;
+            this.btnStart.Location = new System.Drawing.Point(329, 319);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(96, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start Live Video!";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 324);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Select Camera:";
+            // 
+            // cbCamIndex
+            // 
+            this.cbCamIndex.FormattingEnabled = true;
+            this.cbCamIndex.Items.AddRange(new object[] {
+            "0",
+            "1"});
+            this.cbCamIndex.Location = new System.Drawing.Point(218, 321);
+            this.cbCamIndex.Name = "cbCamIndex";
+            this.cbCamIndex.Size = new System.Drawing.Size(64, 21);
+            this.cbCamIndex.TabIndex = 5;
+            this.cbCamIndex.Text = "NONE";
+            this.cbCamIndex.SelectedIndexChanged += new System.EventHandler(this.cbCamIndex_SelectedIndexChanged);
+            // 
+            // comboBoxScIncRte
+            // 
+            this.comboBoxScIncRte.FormattingEnabled = true;
+            this.comboBoxScIncRte.Items.AddRange(new object[] {
+            "1.1",
+            "1.2",
+            "1.3",
+            "1.4"});
+            this.comboBoxScIncRte.Location = new System.Drawing.Point(138, 22);
+            this.comboBoxScIncRte.Name = "comboBoxScIncRte";
+            this.comboBoxScIncRte.Size = new System.Drawing.Size(54, 21);
+            this.comboBoxScIncRte.TabIndex = 6;
+            this.comboBoxScIncRte.Text = "1.1";
+            // 
+            // comboBoxMinNeigh
+            // 
+            this.comboBoxMinNeigh.FormattingEnabled = true;
+            this.comboBoxMinNeigh.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4"});
+            this.comboBoxMinNeigh.Location = new System.Drawing.Point(138, 49);
+            this.comboBoxMinNeigh.Name = "comboBoxMinNeigh";
+            this.comboBoxMinNeigh.Size = new System.Drawing.Size(54, 21);
+            this.comboBoxMinNeigh.TabIndex = 7;
+            this.comboBoxMinNeigh.Text = "2";
+            // 
+            // textBoxWinSize
+            // 
+            this.textBoxWinSize.Location = new System.Drawing.Point(138, 76);
+            this.textBoxWinSize.Name = "textBoxWinSize";
+            this.textBoxWinSize.Size = new System.Drawing.Size(54, 20);
+            this.textBoxWinSize.TabIndex = 8;
+            this.textBoxWinSize.Text = "25";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Scale Increase Rate:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Min Neighbours:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 26);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Min Detection Scale\r\n(Window Size)";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(12, 319);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(92, 23);
+            this.btnBrowse.TabIndex = 12;
+            this.btnBrowse.Text = "Browse Image";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(554, 314);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 14;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // groupBoxTuneDetectParam
+            // 
+            this.groupBoxTuneDetectParam.Controls.Add(this.label2);
+            this.groupBoxTuneDetectParam.Controls.Add(this.label3);
+            this.groupBoxTuneDetectParam.Controls.Add(this.label4);
+            this.groupBoxTuneDetectParam.Controls.Add(this.textBoxWinSize);
+            this.groupBoxTuneDetectParam.Controls.Add(this.comboBoxScIncRte);
+            this.groupBoxTuneDetectParam.Controls.Add(this.comboBoxMinNeigh);
+            this.groupBoxTuneDetectParam.Location = new System.Drawing.Point(431, 12);
+            this.groupBoxTuneDetectParam.Name = "groupBoxTuneDetectParam";
+            this.groupBoxTuneDetectParam.Size = new System.Drawing.Size(200, 121);
+            this.groupBoxTuneDetectParam.TabIndex = 15;
+            this.groupBoxTuneDetectParam.TabStop = false;
+            this.groupBoxTuneDetectParam.Text = "Tune Detection Parameters:";
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Enabled = false;
+            this.btnPrev.Location = new System.Drawing.Point(431, 276);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(42, 23);
+            this.btnPrev.TabIndex = 16;
+            this.btnPrev.Text = "<";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            this.btnPrev.Click += new System.EventHandler(this.btnPrev_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Enabled = false;
+            this.btnNext.Location = new System.Drawing.Point(479, 276);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(42, 23);
+            this.btnNext.TabIndex = 17;
+            this.btnNext.Text = ">";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // pbExtractedFaces
+            // 
+            this.pbExtractedFaces.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbExtractedFaces.Location = new System.Drawing.Point(431, 163);
+            this.pbExtractedFaces.Name = "pbExtractedFaces";
+            this.pbExtractedFaces.Size = new System.Drawing.Size(90, 107);
+            this.pbExtractedFaces.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbExtractedFaces.TabIndex = 18;
+            this.pbExtractedFaces.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(431, 147);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Extracted Face:";
+            // 
+            // CameraCapture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 351);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pbExtractedFaces);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrev);
+            this.Controls.Add(this.groupBoxTuneDetectParam);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.cbCamIndex);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.CamImageBox);
+            this.Name = "CameraCapture";
+            this.Text = "Camera Output";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CamImageBox)).EndInit();
+            this.groupBoxTuneDetectParam.ResumeLayout(false);
+            this.groupBoxTuneDetectParam.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbExtractedFaces)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox CamImageBox;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbCamIndex;
+        private System.Windows.Forms.ComboBox comboBoxScIncRte;
+        private System.Windows.Forms.ComboBox comboBoxMinNeigh;
+        private System.Windows.Forms.TextBox textBoxWinSize;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.GroupBox groupBoxTuneDetectParam;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.PictureBox pbExtractedFaces;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ FaceRecognitionTest/FaceRecognitionTest/CameraCapture.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using Emgu.CV.CvEnum;
+
+namespace FaceRecognitionTest
+{
+    public partial class CameraCapture : Form
+    {
+        //*****************************************************************
+        //                          Global Variables
+        //*****************************************************************
+        private Capture capture;        //takes images from camera as image frames
+        
+        Image<Bgr, byte> ImageFrame;    //The Global "Input Image"
+
+        //***************For Face Detector(Haar Classifier)****************
+        private HaarCascade haar;       //the viola-jones classifier (detector)
+
+        //Set the Default values of the parameters
+        //to be used as a variable in call to DetectHaarCascade()
+        private int WindowsSize = 25;
+        private Double ScaleIncreaseRate = 1.1;
+        private int MinNeighbors = 3;
+
+        Bitmap[] ExtFaces;
+        int faceNo = 0;
+
+        //*****************************************************************
+        //*****************************************************************
+        
+        public CameraCapture()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // adjust path to find your XML file 
+            haar = new HaarCascade("haarcascade_frontalface_default.xml");
+        }
+
+        //------------------------------------------------------------------------------//
+        //Process Frame() below is our user defined function in which we will create an EmguCv 
+        //type image called ImageFrame. capture a frame from camera and allocate it to our 
+        //ImageFrame. then show this image in ourEmguCV imageBox
+        //------------------------------------------------------------------------------//
+        private void ProcessFrame(object sender, EventArgs arg)
+        {
+            //fetch the frame captured by web camera
+            ImageFrame = capture.QueryFrame();
+
+            //show the image in the EmguCV ImageBox
+            CamImageBox.Image = ImageFrame;
+            
+            //Read an image from hard disk at location e.g ‘E:\IMAGES\’
+            //Image InputImg = Image.FromFile(@"C:\Users\user\Pictures\ManyFacesImage.jpg");
+            //Image<Bgr, byte> ImageFrame = new Image<Bgr, byte>(new Bitmap(InputImg));
+            
+            //Below 2 lines of code capture image from webcam
+            //Image<Bgr, Byte> ImageFrame = capture.QueryFrame();
+            //CamImageBox.Image = ImageFrame;
+            //ImageFrame.Save(@"C:\Users\user\Desktop\IS Thesis Code\FaceRecognitionTest\FaceRecognitionTest\Image.jpg");
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (capture != null)
+            {
+                if (btnStart.Text == "Detect Face")
+                {  //if camera is getting frames then pause the capture and set button Text to
+                    // "Resume" for resuming capture
+                    btnStart.Text = "Resume Live Video"; //
+
+                    //Pause the live streaming video
+                    Application.Idle -= ProcessFrame;
+
+                    //Call face detection
+                    DetectFaces();
+                }
+                else
+                {
+                    //if camera is NOT getting frames then start the capture and set button
+                    // Text to "Pause" for pausing capture
+                    btnStart.Text = "Detect Face";
+                    Application.Idle += ProcessFrame;
+                }
+            }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Image InputImg = Image.FromFile(openFileDialog.FileName);
+                ImageFrame = new Image<Bgr, byte>(new Bitmap(InputImg));
+                CamImageBox.Image = ImageFrame;
+                DetectFaces();
+            }
+        }
+
+        //Allows user to select a desired camera and starts it
+        private void cbCamIndex_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //Set the camera number to the one selected via combo box
+            int CamNumber = -1;
+            CamNumber = int.Parse(cbCamIndex.Text);
+
+            //Start the selected camera
+            #region if capture is not created, create it now
+            if (capture == null)
+            {
+                try
+                {
+                    capture = new Capture(CamNumber);
+                }
+                catch (NullReferenceException excpt)
+                {
+                    MessageBox.Show(excpt.Message);
+                }
+            }
+            #endregion
+
+            //Start showing the stream from camera
+            btnStart_Click(sender, e);
+            btnStart.Enabled = true;   //enable the button for pause/resume
+        }
+
+        private void DetectFaces()
+        {
+            //convert the image to gray scale
+            Image<Gray, byte> grayframe = ImageFrame.Convert<Gray, byte>();
+
+            //Assign user-defined Values to parameter variables:
+            MinNeighbors = int.Parse(comboBoxMinNeigh.Text); //the 3rd parameter
+            WindowsSize = int.Parse(textBoxWinSize.Text); //the 5th parameter
+            ScaleIncreaseRate = Double.Parse(comboBoxScIncRte.Text); //the 2nd parameter
+
+            //Detect faces from the gray-scale image and store into an array of type 'var',i.e 'MCvAvgComp[]'
+            var faces = grayframe.DetectHaarCascade(haar, ScaleIncreaseRate, MinNeighbors,
+                                    HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+                                    new Size(WindowsSize, WindowsSize))[0];
+            if (faces.Length > 0)
+            {
+                MessageBox.Show("Total Faces Detected: " + faces.Length.ToString());
+                Bitmap BmpInput = grayframe.ToBitmap();
+                Bitmap ExtractedFace;   //empty
+                Graphics FaceCanvas;
+                ExtFaces = new Bitmap[faces.Length];
+
+                //draw a green rectangle on each detected face in image
+                foreach (var face in faces)
+                {
+                    ImageFrame.Draw(face.rect, new Bgr(Color.Green), 3);
+
+                    //set the size of the empty box(ExtractedFace) which will later contain the detected face
+                    ExtractedFace = new Bitmap(face.rect.Width, face.rect.Height);
+
+                    //set image as FaceCanvas, for painting
+                    FaceCanvas = Graphics.FromImage(ExtractedFace);
+
+                    //Taken detected face and copied it into ExtractedFace
+                    FaceCanvas.DrawImage(BmpInput, 0, 0, face.rect, GraphicsUnit.Pixel);
+
+                    ExtFaces[faceNo] = ExtractedFace;
+                    faceNo++;
+                }
+                faceNo = 0;
+                pbExtractedFaces.Image = ExtFaces[faceNo];
+
+                //Display the detected faces in imagebox
+                CamImageBox.Image = ImageFrame;    //CamImageBox is an EmguCV imageBox
+
+                btnNext.Enabled = true;
+                btnPrev.Enabled = true;
+            }
+        }
+
+        private void ReleaseData()
+        {
+            if (capture != null)
+                capture.Dispose();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (faceNo < ExtFaces.Length - 1)
+            {
+                faceNo++;
+                pbExtractedFaces.Image = ExtFaces[faceNo];
+            }
+            else
+                MessageBox.Show("Last image!");
+        }
+
+        private void btnPrev_Click(object sender, EventArgs e)
+        {
+            if (faceNo > 0)
+            {
+                faceNo--;
+                pbExtractedFaces.Image = ExtFaces[faceNo];
+            }
+            else
+                MessageBox.Show("1st image!");
+        }
+    }
+}
